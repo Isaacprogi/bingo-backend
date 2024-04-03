ac6b0e966080080ef19fd72b1a8d36e6d3832114
Isaac <isaaconyes80@gmail.com>
2024-04-03 23:31:02 +0100

    updated logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -78,9 +78,7 @@ const login = async (req, res, next) => {
 
 // Logout user
 const logout = (req, res, next) => {
-    const logout = (req, res, next) => {
-        res.clearCookie('ref', { httpOnly: true, secure: true, sameSite: 'None', path: '/' });
-    };
+    res.clearCookie('ref', { httpOnly: true, secure: true, sameSite: 'None', path: '/' });
 };
 
 
